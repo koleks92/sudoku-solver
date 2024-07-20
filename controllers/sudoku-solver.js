0759547f2afd9fda7a43d5011edb8c327723d74a
Jan Konieczek <koleks92@gmail.com>
2024-07-20 18:51:48 +0000

    feat: validate function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,16 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    if (puzzleString.length != 81) {
+      return false;
+    } else {
+      const regex = /^[0-9.]+$/;
+      if (regex.test(puzzleString)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
