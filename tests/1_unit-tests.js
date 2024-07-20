3f3b1a6da46da600046c282885cdcd824a3df5b0
Jan Konieczek <koleks92@gmail.com>
2024-07-20 19:02:40 +0000

    feat: validate tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,19 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const SudokuSolver = require('../controllers/sudoku-solver.js');
+let solver = new SudokuSolver;
 
 suite('Unit Tests', () => {
-
+    suite('Functions to check sudoku-solver.js', function() {
+        test('Logic handles a valid puzzle string of 81 characters', function() {
+            assert.equal(solver.validate('..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'), true);
+        });
+        test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', function() {
+            assert.equal(solver.validate('ad9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'), false);
+        });
+        test('Logic handles a puzzle string that is not 81 characters in length', function() {
+            assert.equal(solver.validate('...9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..'), false);
+        });
+    })
 });
