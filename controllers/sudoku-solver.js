e779e33d845697fc111d7cf70b9a93555496d7d0
Jan Konieczek <koleks92@gmail.com>
2024-07-21 06:39:49 +0000

    feat: checkRowPlacement

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,8 +1,8 @@
 
 // Check if row/value is correct
-function rowValueCheck(row) {
+function columnValueCheck(number) {
   const valid = '123456789';
-  if (valid.includes(row)) {
+  if (valid.includes(number)) {
     return true;
   } else {
     return false;
@@ -10,9 +10,9 @@ function rowValueCheck(row) {
 };
 
 // Check if column is correct
-function columnCheck(column) {
+function rowCheck(row) {
   const valid = 'ABCDEFGHI';
-  if (valid.includes) {
+  if (valid.includes(row)) {
     return true;
   } else {
     return false;
@@ -21,9 +21,10 @@ function columnCheck(column) {
 
 // Check if everything is correct(row, column, value)
 function checkCorrect(row, column, value) {
-  if (rowValueCheck(row) && columnCheck(column) && rowValueCheck(value)){
+  if (columnValueCheck(column) && rowCheck(row) && columnValueCheck(value)){
     return true;
   } else {
+    console.log("check")
     return false;
   }
 }
@@ -36,7 +37,7 @@ class SudokuSolver {
     if (puzzleString.length != 81) {
       return false;
     } else {
-      const regex = /^[0-9.]+$/;
+      const regex = /^[1-9.]+$/;
       if (regex.test(puzzleString)) {
         return true;
       } else {
@@ -48,25 +49,46 @@ class SudokuSolver {
   checkRowPlacement(puzzleString, row, column, value) {
     // Check if data is correct
     if (checkCorrect(row, column, value)) {
-      // Check if already placed
+      // Get substring for row
       let substring;
       switch(row) {
-        case 1: substring = puzzleString.substring(0,9);
-        case 2: substring = puzzleString.substring(9,18);
-        case 3: substring = puzzleString.substring(18,27);
-        case 4: substring = puzzleString.substring(27,36);
-        case 5: substring = puzzleString.substring(36,45);
-        case 6: substring = puzzleString.substring(45,54);
-        case 7: substring = puzzleString.substring(54,63);
-        case 8: substring = puzzleString.substring(63,72);
-        case 9: substring = puzzleString.substring(72,81);
+        case 'A': 
+          substring = puzzleString.substring(0,9)
+          break;
+        case 'B': 
+          substring = puzzleString.substring(9,18)
+          break;
+        case 'C': 
+          substring = puzzleString.substring(18,27)
+          break;
+        case 'D':
+          substring = puzzleString.substring(27,36)
+          break;
+        case 'E': 
+          substring = puzzleString.substring(36,45)
+          break;
+        case 'F': 
+          substring = puzzleString.substring(45,54)
+          break;
+        case 'G': 
+          substring = puzzleString.substring(54,63)
+          break;
+        case 'H': 
+          substring = puzzleString.substring(63,72)
+          break;
+        case 'I': 
+          substring = puzzleString.substring(72,81)
+          break;
       };
 
-      if (substring[column] != ".") {
-        return true;
-      } else {
+      // Check if possible to place
+      if (substring[column] != "." || substring.includes(value)) {
         return false;
+      } else {
+        return true;
       }
+    } else{
+      return false;
     }
   }
 
